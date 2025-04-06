e6eeab18f6c3570b4bfcfa491d9047c456ea1fb3
ourstory251203 <ourstory251203@gmail.com>
2025-04-06 11:16:09 +0530

    server.js

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const request = require('request');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 app.use(cors());
 
 app.get('/proxy', (req, res) => {
